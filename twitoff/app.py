2ea6025cffd472a486af89f188a62172bae8d0bc
Connor <connorpheraty@gmail.com>
2019-06-20 16:32:28 -0500

    Changed routing code in the create_app function from local to heroku

--- twitoff/app.py
+++ twitoff/app.py
@@ -7,9 +7,9 @@ from .predict2 import predict_user
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('HEROKU_POSTGRESQL_JADE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['ENV'] = config('ENV') # TODO change before deploying
+   
     DB.init_app(app)
 
     @app.route('/')
