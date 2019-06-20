3867e12f5037bde29a1d0b81437ee4ade6476d9c
Connor <connorpheraty@gmail.com>
2019-06-20 14:27:43 -0500

    Added comparing function, updated base.html file

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,6 +2,7 @@ from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
 from .twitter import add_or_update_user
+from .predict2 import predict_user
 
 def create_app():
     """Create and configure an instance of the Flask application."""
@@ -29,6 +30,20 @@ def create_app():
             message = "Error adding {}: {}".format(name, e)
             tweets = []
         return render_template("user.html", title=name, tweets=tweets, message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                               request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2,
+                                      request.values['tweet_text'])
+        message = '"{}" is more likely to be said by {} than {}'.format(
+            request.values['tweet_text'], user1 if prediction else user2,
+            user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
     
     @app.route('/reset')
     def reset():
--- twitoff/predict2.py
+++ twitoff/predict2.py
@@ -8,9 +8,9 @@ from .twitter import BASILICA
 def predict_user(user1_name, user2_name, tweet_text, cache=None):
   """Determine and return which user is more likely to say a given Tweet."""
   user1 = User.query.filter(User.name == user1_name).one()
-  user2 = User.query.filter(User.name == user1_name).two()
-  user1_embeddings = np.array([tweet.embeddings for tweet in user1.tweets])
-  user2_embeddings = np.array([tweet.embeddings for tweet in user2.tweets])
+  user2 = User.query.filter(User.name == user2_name).one()
+  user1_embeddings = np.array([tweet.embedding for tweet in user1.tweets])
+  user2_embeddings = np.array([tweet.embedding for tweet in user2.tweets])
   embeddings = np.vstack([user1_embeddings, user2_embeddings])
   labels = np.concatenate([np.ones(len(user1.tweets)),
                           np.zeros(len(user2.tweets))])
